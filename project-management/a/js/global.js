f6ce7ec82f4045af140171bea920e86d6648a87a
vincentclee <vincentlee.dolbydigital@yahoo.com>
2014-07-28 15:06:47 -0500

    Test 2

--- project-management/a/js/global.js
+++ project-management/a/js/global.js
@@ -42,23 +42,17 @@ function popup(message) {
 // Whereas .serializeArray() serializes a form into an array, .serializeObject()
 // serializes a form into an (arguably more useful) object.
 
-(function($,undefined){
-  '$:nomunge'; // Used by YUI compressor.
-  
-  $.fn.serializeObject = function(){
-    var obj = {};
-    
-    $.each( this.serializeArray(), function(i,o){
-      var n = o.name,
-        v = o.value;
-        
-        obj[n] = obj[n] === undefined ? v
-          : $.isArray( obj[n] ) ? obj[n].concat( v )
-          : [ obj[n], v ];
-    });
-    
-    return obj;
-  };
-  
-})(jQuery);
-
+$.fn.serializeObject = function(){
+	var obj = {};
+	
+	$.each( this.serializeArray(), function(i,o){
+		var n = o.name,
+		v = o.value;
+		
+		obj[n] = obj[n] === undefined ? v
+				: $.isArray( obj[n] ) ? obj[n].concat( v )
+						: [ obj[n], v ];
+	});
+	
+	return obj;
+};
