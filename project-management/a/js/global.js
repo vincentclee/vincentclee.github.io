04c2e22ef77d83a9477a5ce2eaedf90ed6713691
vincentclee <vincentlee.dolbydigital@yahoo.com>
2014-07-28 15:33:42 -0500

    Changes

--- project-management/a/js/global.js
+++ project-management/a/js/global.js
@@ -29,30 +29,3 @@ function popup(message) {
 	$(".kdnotification").show();
 	$(".kdnotification").delay(POPUP_DURATION).fadeOut();
 }
-
-/*!
- * jQuery serializeObject - v0.2 - 1/20/2010
- * http://benalman.com/projects/jquery-misc-plugins/
- * 
- * Copyright (c) 2010 "Cowboy" Ben Alman
- * Dual licensed under the MIT and GPL licenses.
- * http://benalman.com/about/license/
- */
-
-// Whereas .serializeArray() serializes a form into an array, .serializeObject()
-// serializes a form into an (arguably more useful) object.
-
-$.fn.serializeObject = function(){
-	var obj = {};
-	
-	$.each( this.serializeArray(), function(i,o){
-		var n = o.name,
-		v = o.value;
-		
-		obj[n] = obj[n] === undefined ? v
-				: $.isArray( obj[n] ) ? obj[n].concat( v )
-						: [ obj[n], v ];
-	});
-	
-	return obj;
-};
